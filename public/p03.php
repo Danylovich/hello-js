b20f6fac3ec951f75d9e55e1314208d4b8884181
ВашÐandriu922 <gladiuka@ukr.net>
2019-11-03 17:50:47 +0200

    Lesson08

---
+++ public/p03.php
@@ -0,0 +1,11 @@
+<?php
+
+    require_once '../vendor/autoload.php';
+    $loader = new \Twig\Loader\FilesystemLoader(
+        '../public/html'
+    );
+    $twig = new \Twig\Environment($loader);
+
+    echo $twig->render('app.html');
+
+?>

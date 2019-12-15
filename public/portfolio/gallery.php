c40b84278590f041514fbfa1bac9d8ba18bb8efa
ВашÐandriu922 <gladiuka@ukr.net>
2019-12-15 17:55:40 +0200

    Messege

--- public/portfolio/gallery.php
+++ public/portfolio/gallery.php
@@ -6,7 +6,20 @@ $loader = new \Twig\Loader\FilesystemLoader(
 );
 $twig = new \Twig\Environment($loader);
 
-require_once '../storage/hotels_dat.php';
+// require_once '../storage/hotels_dat.php';
+
+// $h_str = serialize( $hotels );
+// // var_dump( $h_str );
+// $f = fopen( '../storage/hotals.dat', 'w+' );
+// $ws = fwrite( $f, $h_str );
+// fclose( '../storage/hotals.dat', $f );
+
+$fname = '../storage/hotels.txt';
+$f = fopen($fname,'r');
+$h_str = fread( $f, filesize( $fname ) );
+$hotels = unserialize( $h_str );
+fclose( $f );
+
 
 echo $twig->render('gallery.html',
     [ 'hotels' => 
